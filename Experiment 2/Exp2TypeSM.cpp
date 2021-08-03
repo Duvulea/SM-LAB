2e09e7bb3afc88a4cb8ce14d8c28d38b96f060e3
Duvulea <54505667+Duvulea@users.noreply.github.com>
2021-08-03 18:15:12 +0530

    Update Exp2TypeSM.cpp

--- Experiment 2/Exp2TypeSM.cpp
+++ Experiment 2/Exp2TypeSM.cpp
@@ -4,18 +4,14 @@
 using namespace std;
 
 int generate_demand()
-{
-    //initialize random seed
+{ 
     srand(time(NULL));
-    //generate random number between 0 to 99
     return rand() % 100;
 }
 int main()
 {
     int P, Q, stock, today, dueDate, unitsDue, startDate, demand, reorderCost;
     float total_cost;
-
-    //For different values of P and Q
     for(int i=0;i<5;i++)
     {
         cin>>P>>Q; //125 150
@@ -31,28 +27,24 @@ int main()
 
         while(today<=180)
         {
-            //If due date, refill stock
             if(today==dueDate)
             {
                 stock+= Q;
                 unitsDue=0;
-            }
-            //Today's demand of orders
+            }       
             demand = generate_demand();
-
-            //If demand less than stock, add carrying cost
             if(demand<=stock)
             {
                 stock-= demand;
                 total_cost+= stock*0.75;
             }
-            //If demand more than stock, add lost sale cost
+     
             else
             {
                 total_cost+= (demand - stock)*18;
                 stock = 0;
             }
-            //If stock less than minimum level, restock order
+          
             if(stock + unitsDue <= P)
             {
                 unitsDue = Q;
@@ -61,9 +53,7 @@ int main()
             }
             today++;
         }
-
-        cout<<"Total Cost for P= "<<P<<" and Q= "<<Q<<" is "<<total_cost<<"\n";
-
+       cout<<"Total Cost for P= "<<P<<" and Q= "<<Q<<" is "<<total_cost<<"\n";
     }
     return 0;
 }
